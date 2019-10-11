560b277a1c8b687b31aaab0f4ecbacafd38ffd04
Azra Animating <42267730+AzraAnimating@users.noreply.github.com>
2019-10-11 14:54:33 +0200

    Update Autobahn.java

--- Autobahn.java
+++ Autobahn.java
@@ -1,4 +1,37 @@
-
+import java.util.*;
+public class Autobahn
+{
+    Queue<Auto> autosRechts = new Queue<Auto>();
+    Queue<Auto> autosLinks = new Queue<Auto>();
+    Queue<Auto> autosMitte = new Queue<Auto>();
+    Queue<Auto> wunschAutos = new Queue<Auto>();
+    Queue<Auto> wunschAutosClone = new Queue<Auto>();
+    GUI_project display = new GUI_project(this);
+    int aAnzahlAutos;
+    
+    public void autoErstellenLinks(String pKennzeichen)
+    {
+        autosLinks.enqueue(new Auto(pKennzeichen));
+        display.addAutoLinks(pKennzeichen);
+    }
+    public void autoErstellenRechts(String pKennzeichen)
+    {
+        autosRechts.enqueue(new Auto(pKennzeichen));
+        display.addAutoRechts(pKennzeichen);
+    }
+  
+    /*public void autosErstellen(int anzahlAutos)
+    {
+         for(int i = 0; i < anzahlAutos+1; i++)
+         {
+                autosRechts.enqueue(new Auto("HA-" + i));
+                
+                autosLinks.enqueue(new Auto("EN-" + i));
+         }  
+         aAnzahlAutos = anzahlAutos;
+         this.wunsch();
+    }*/
+    /*
     public void autosErstellenLinks(int anzahlAutos)
     {
          for(int i = 0; i < anzahlAutos+1; i++)
